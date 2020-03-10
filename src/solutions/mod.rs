dd4aade39fda89d908daaf7c4c025a5aa88467c7
Sen Na <1439468790@qq.com>
2020-03-10 14:11:42 +0800

    Finish p543

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -1,9 +1,10 @@
-#![allow(dead_code)]
+#![allow(dead_code, unused_macros)]
 
 mod p121;
 mod p307;
 mod p313;
 mod p322;
 mod p456;
+mod p543;
 mod p687;
 mod p876;
---
+++ src/solutions/p543.rs
@@ -0,0 +1,39 @@
+use crate::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+struct Solution;
+
+type Tree = Option<Rc<RefCell<TreeNode>>>;
+
+static mut MAX_LENGTH: i32 = 0;
+
+impl Solution {
+    pub fn diameter_of_binary_tree(root: Tree) -> i32 {
+        fn max_depth(node: &Tree) -> i32 {
+            match node {
+                Some(node) => {
+                    let node = node.borrow();
+                    let left = max_depth(&node.left);
+                    let right = max_depth(&node.right);
+                    unsafe {
+                        if left + right > MAX_LENGTH {
+                            MAX_LENGTH = left + right;
+                        }
+                    }
+                    1 + std::cmp::max(left, right)
+                }
+                None => 0,
+            }
+        };
+
+        max_depth(&root);
+        unsafe { MAX_LENGTH }
+    }
+}
+
+#[test]
+fn simple_none() {
+    let root = None;
+    assert_eq!(Solution::diameter_of_binary_tree(root), 0);
+}
