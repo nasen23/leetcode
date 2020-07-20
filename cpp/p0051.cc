c0c768c9d294ffe7cd5f3c31d5d8eca8cfb2b3de
Sen Na <i@nasen.xyz>
2020-07-20 23:12:10 +0800

    Finish p0051

--- cpp/p0051.cc
+++ cpp/p0051.cc
@@ -1,50 +1,37 @@
 #include "x.h"
 
 class Solution {
+  int mask;
+
 public:
   vector<vector<string>> solveNQueens(int n) {
+    mask = (1 << n) - 1;
     vector<vector<string>> res;
     vector<int> put(n, 0);
-    function<void(int)> dfs = [&](int r) {
-      if (!r) {
-        vector<string> ans;
-        for (int i = 1; i <= n; i++) {
-          string row;
-          for (int j = 0; j < n; j++) {
-            if (put[j] == i) {
-              row.push_back('Q');
-            } else {
-              row.push_back('.');
-            }
-          }
-          ans.push_back(row);
+    function<void(int, int, int, int)> dfs = [&](int row, int l, int r, int u) {
+      if (row == n) {
+        vector<string> ans(n, string(n, '.'));
+        for (int i = 0; i < n; i++) {
+          ans[i][put[i]] = 'Q';
         }
         res.push_back(ans);
         return;
       }
-      for (int i = 0; i < n; i++) {
-        if (put[i]) {
-          continue;
+      int bitmap = mask & ~(l | r | u);
+      while (bitmap) {
+        int bit = bitmap & -bitmap;
+        int t = bit;
+        int c = 0;
+        while (t) {
+          t >>= 1;
+          c++;
         }
-        bool cont = false;
-        for (int j = 1; j <= n - r; j++) {
-          if ((i >= j && put[i - j] == r + j) ||
-              (i < n - j && put[i + j] == r + j)) {
-            cont = true;
-            break;
-          }
-        }
-        if (cont) {
-          continue;
-        }
-        put[i] = r;
-        dfs(r - 1);
-        put[i] = 0;
+        put[row] = c - 1;
+        dfs(row + 1, (l | bit) << 1, (r | bit) >> 1, u | bit);
+        bitmap ^= bit;
       }
     };
-    dfs(n);
+    dfs(0, 0, 0, 0);
     return res;
   }
 };
-
-int main(int argc, char *argv[]) { return 0; }
