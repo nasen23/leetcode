8b35c91fca113fa06b7f453dbff3e116ade9bca4
Sen Na <i@nasen.xyz>
2020-08-17 09:51:40 +0800

    Finish p0110

---
+++ src/solutions/p0110.rs
@@ -0,0 +1,27 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn is_balanced(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        is_balance_impl(root).1
+    }
+}
+
+fn is_balance_impl(tree: Option<Rc<RefCell<TreeNode>>>) -> (i32, bool) {
+    match tree {
+        Some(node) => {
+            let node = Rc::try_unwrap(node).unwrap().into_inner();
+            let (d1, b1) = is_balance_impl(node.left);
+            let (d2, b2) = is_balance_impl(node.right);
+            if b1 && b2 && (d1 - d2).abs() <= 1 {
+                (d1.max(d2) + 1, true)
+            } else {
+                (0, false)
+            }
+        }
+        None => (0, true)
+    }
+}
+
+use crate::TreeNode;
+struct Solution;
